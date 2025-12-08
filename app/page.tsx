c62549b214b40c3700ea39b294783aa2f89604aa
XLLAMAA <jaimeadanez11@gmail.com>
2025-12-08 21:07:56 +0100

    feat: Base de la pagina principal, conectandose a spotify mandandole los permisos necesarios y construyo la url de login

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,47 @@
-import Image from "next/image";
+/*Esta es la pagina principal*/
 
-export default function Home() {
+"use client";
+
+import { JSX } from "react";
+
+/*Permisos que le pido a la aplicacion de soptify*/ 
+const PermisosSpotify =[
+  "user-read-email",
+  "user-read-private",
+  "playlist-read-private",
+  "playlist-modify-private",
+  "playlist-modify-public",
+].join(" ");  /*El .join los junta en una array*/
+
+const urlSpotify = "https://accounts.spotify.com/authorize"; 
+
+/* Construye la url de login para spotify con los parametros necesarios*/
+function buildLoginUrl(): string {
+  const  idCliente = process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_ID;
+  const redirectUri = process.env.NEXT_PUBLIC_REDIRECT_URI;
+
+  const parametros = new URLSearchParams ({
+    client_id: idCliente ?? "",
+    response_type: "code",
+    redirect_uri: redirectUri ?? "",
+    scope: PermisosSpotify,
+  });
+
+  return `${urlSpotify}?${parametros.toString()}`;
+}
+export default function Home(): JSX.Element {
+  const handleLogin = () => {
+    const url = buildLoginUrl();
+    window.location.href = url
+  };
+
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
+    <main className="min-h-screen flex flex-col items-center justify-center bg-black text-green-600">
+      <h1 className="text-3xl font-bold mb-4">Spotify Taste Mixer</h1>
+      <p className="mb-6 text-center max-w-md">Aplicacion web para generar playlist personalizadas en funcion de tus gustos, tanto de artistas, generos...</p>
+      <button onClick={(handleLogin)} 
+      className="px-6 py-3 rounded-full bg-green-900 hover:opacity-90 hover:bg-green-600">🎧</button>
+    </main>
+    
   );
 }
